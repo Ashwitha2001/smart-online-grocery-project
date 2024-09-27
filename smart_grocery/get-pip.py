5c1646b56ff87b77bc6276aec84dad74d8a8844a
Ashwitha2001 <ashwithar2001@gmail.com>
2024-09-27 15:04:22 +0530

    Remove non-binary files from Git LFS and re-add them

---
+++ smart_grocery/get-pip.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bc37786ec99618416cc0a0ca32833da447f4d91ab51d2c138dd15b7af21e8e9a
+size 2266947
--- smart_grocery/manage.py
+++ smart_grocery/manage.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2e57a8b015dd3abc07d075abdfdb92c0b094c0e9cc1f3c487b10b82a8ecec5c
-size 691
+oid sha256:377a5908154980010bde9143e4750854361e2eb83dfe0903c62461ed6556cacf
+size 687
